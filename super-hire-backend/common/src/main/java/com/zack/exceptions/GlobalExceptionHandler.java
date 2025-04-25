dc3386b98a9e435cee1fdf9d00cda6674087663d
# <#>
2025-04-25 22:08:36 +0800

    修复全局处理异常bug

--- super-hire-backend/common/src/main/java/com/zack/exceptions/GlobalExceptionHandler.java
+++ super-hire-backend/common/src/main/java/com/zack/exceptions/GlobalExceptionHandler.java
@@ -4,8 +4,9 @@ import com.zack.common.CommonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
