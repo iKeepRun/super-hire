65e6944a23ac8903428da1dfb876929836b02242
czq <592012072@qq.com>
2025-04-28 13:10:15 +0800

    common公用模块添加对jwttoken全局异常处理，用于对与gateway放行的接口进行处理

--- super-hire-backend/common/src/main/java/com/zack/exceptions/GlobalExceptionHandler.java
+++ super-hire-backend/common/src/main/java/com/zack/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.zack.exceptions;
 
 import com.zack.common.CommonResult;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +36,19 @@ public class GlobalExceptionHandler {
         return CommonResult.error(errors);
     }
 
+
+    @ExceptionHandler({
+            SignatureException.class,
+            ExpiredJwtException.class,
+            UnsupportedJwtException.class,
+            MalformedJwtException.class,
+            io.jsonwebtoken.security.SignatureException.class
+    })
+    @ResponseBody
+    public CommonResult returnSignatureException(SignatureException e) {
+        e.printStackTrace();
+        return CommonResult.error(ErrorCode.JWT_SIGNATURE_ERROR);
+    }
 
 
     @ExceptionHandler(RuntimeException.class)
