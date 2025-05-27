aebee17fc1232b2f7c6bd2a4315d44d8d735aae3
# <#>
2025-05-27 20:12:08 +0800

    新增minio上传文件工具类

---
+++ super-hire-backend/service-file/src/main/java/com/zack/config/MinIOConfig.java
@@ -0,0 +1,27 @@
+package com.zack.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class MinIOConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+    @Value("${minio.bucketName}")
+    private String bucketName;
+    @Value("${minio.fileHost}")
+    private String fileHost;
+
+    @Bean
+    public MinIOUtils createMinioClient(){
+        return new MinIOUtils(endpoint,fileHost,bucketName,accessKey,secretKey);
+    }
+}
